6345a571bec903baa93dcef9b5da38ce53170ff1
claudio junior <claudiodsesjunior@gmail.com>
2026-01-28 10:28:55 -0300

    feat: created todo controller + mocks

---
+++ Tafera.Api/Controllers/TodoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tafera.Application.Todos;
+using Tafera.Domain.Models.Todos;
+
+namespace Tafera.Api.Controllers;
+
+[ApiController]
+[Route("api/todo")]
+public class TodoController : Controller
+{
+    public TodoItemsMockList TodoItemsMock = new TodoItemsMockList();
+
+    public List<TodoItem> TodoItems;
+
+    public TodoController()
+    {
+        TodoItems = TodoItemsMock.GetTodoItemsMockList();
+    }
+
+    [HttpGet("hello-world")]
+    public string GetHelloWorld()
+    {
+        return "Hello World";
+    }
+
+    [HttpGet()]
+    public List<TodoItem> GetTodoItems() 
+    {
+        return TodoItems;
+    }
+
+    [HttpGet("{id:guid}")]
+    public TodoItem GetTodoItem(Guid id)
+    {
+        return TodoItems.First(t => t.Id == id);
+    }
+
+    [HttpPost("create")]
+    public TodoItem CreateTodoItem(string title, string description, Priority priority)
+    {
+        var todoItem = new TodoItem(title, description, priority);
+        TodoItems.Add(todoItem);
+        return todoItem;
+    }
+}
---
+++ Tafera.Application/Todos/TodoItemsMockList.cs
@@ -0,0 +1,72 @@
+﻿using Tafera.Domain.Models.Todos;
+
+namespace Tafera.Application.Todos;
+
+public class TodoItemsMockList
+{
+    public List<TodoItem> GetTodoItemsMockList()
+    {
+        return new List<TodoItem>
+        {
+            new TodoItem(
+                "Estudar fundamentos de .NET",
+                "Revisar Program.cs, DI básica e ciclo de vida",
+                Priority.High
+            ),
+
+            new TodoItem(
+                "Criar endpoint de criação de tarefas",
+                "Implementar POST /todos no controller",
+                Priority.High
+            ),
+
+            new TodoItem(
+                "Criar endpoint de listagem",
+                "Implementar GET /todos",
+                Priority.Medium
+            ),
+
+            new TodoItem(
+                "Refatorar nomes de classes",
+                "Garantir que nomes reflitam bem o domínio",
+                Priority.Low
+            ),
+
+            new TodoItem(
+                "Adicionar endpoint de conclusão",
+                "Implementar PUT /todos/{id}/complete",
+                Priority.Medium
+            ),
+
+            new TodoItem(
+                "Testar fluxo completo no Swagger",
+                "Criar, listar e concluir tarefas",
+                Priority.High
+            ),
+
+            new TodoItem(
+                "Configurar SQLite",
+                "Adicionar persistência com EF Core",
+                Priority.Medium
+            ),
+
+            new TodoItem(
+                "Criar README do projeto",
+                "Explicar objetivo, stack e como rodar",
+                Priority.Low
+            ),
+
+            new TodoItem(
+                "Revisar domínio",
+                "Validar se métodos do TodoItem fazem sentido",
+                Priority.Medium
+            ),
+
+            new TodoItem(
+                "Commitar versão estável",
+                "Criar tag v0.1.0",
+                Priority.High
+            )
+        };
+    }
+}
